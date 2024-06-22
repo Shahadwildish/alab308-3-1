1b2481b214dd8002d0d6fd1af23e82879e834512
shahadwildish <shayalobeidi@gmail.com>
2024-06-22 18:31:02 -0400

    second commit

---
+++ loop.js
@@ -0,0 +1,6 @@
+for (let num =1; num <= 100; num++) {
+    if (num %3 ==0 ) {console.log (num + ' Fizz') }
+    if (num %5 ==0) {console.log (num + ' Buzz')}
+    if (num %3 ==0 && num %5 ==0 ) {console.log ( num + ' Fizz Buzz')} 
+    if (num %3 !==0 && num %5 !==0 ) {console.log (num)}
+}
